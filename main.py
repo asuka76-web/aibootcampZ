02b983b9bf420a05e4c122c35b5c307078ad91a0
asuka76-web <asuka76@gmail.com>
2025-09-10 03:25:26 +0800

    test 2

--- main.py
+++ main.py
@@ -9,6 +9,28 @@ google_cse_id = st.secrets["GOOGLE_CSE_ID"]
 # Create OpenAI client
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
+# Password
+def check_password():  
+    """Returns `True` if the user had the correct password."""  
+    def password_entered():  
+        """Checks whether a password entered by the user is correct."""  
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):  
+            st.session_state["password_correct"] = True  
+            del st.session_state["password"]  # Don't store the password.  
+        else:  
+            st.session_state["password_correct"] = False  
+    # Return True if the passward is validated.  
+    if st.session_state.get("password_correct", False):  
+        return True  
+    # Show input for password.  
+    st.text_input(  
+        "Password", type="password", on_change=password_entered, key="password"  
+    )  
+    if "password_correct" in st.session_state:  
+        st.error("😕 Password incorrect")  
+    return False
+
+
 # --- App Title ---
 st.set_page_config(page_title="AskGov SG - CPF Q&A", layout="wide")
 st.title("🇸🇬 AskGov SG - CPF Policy Q&A")
